39500d3772b8e56af2beb9088fedd68c73cf95da
ExceptionXD <kumarsingh14abhishek@gmail.com>
2020-04-26 20:33:27 +0530

    Added Image Flip Feature

--- app.js
+++ app.js
@@ -8,29 +8,29 @@ function getMovies()
   xhr.open('GET',`http://www.omdbapi.com/?s=${search}&apikey=2fe665da`,true);
   xhr.onload = function(){
 
-    console.log(this.status);
+    // console.log(this.status);
 
     if(this.status===200)
     {
       
       let output ='';
       var movies = JSON.parse(this.response);
-      console.log(movies);
+      console.log(this.response);
 
        movies['Search'].forEach(function(movie){
         output += `
         <br>
-        <div class="col-md-4">
-        <div class="card" style="width: 18rem; heigth: 20rem;">
-          <img class="card-img-top zoom" src="${movie.Poster}" alt="Poster" style="width:=350px;padding-left:=20px;height:=300px;padding-top:=20px;">
-          <div class="card-body " style="height:=300px">
-          <h5 class="card-title">${movie.Title}</h5>
-          </div>
-          <div class="card-footer">
-          <small class="text-muted ">Released Year :${movie.Year}</small>
-          </div>
+        <div class="flip-card" style="margin-bottom: 300px;margin-left: 30px;">
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src="${movie.Poster}" alt="Avatar" style="width:300px;height:450px;">
+            </div>
+          <div class="flip-card-back"  style="width:300px;height:450px;">
+            <h1>${movie.Title}</h1>
+            <p>${movie.Year}</p>
           </div>
       </div>
+    </div>
         `
       })
 
@@ -50,9 +50,11 @@ function getMoviesOnLoad()
   xhr.open('GET',`http://www.omdbapi.com/?s=Avengers&apikey=2fe665da`,true);
   xhr.onload = function(){
 
+    // console.log(this.status);
+
     if(this.status===200)
     {
-
+      
       let output ='';
       var movies = JSON.parse(this.response);
       console.log(movies);
@@ -60,27 +62,25 @@ function getMoviesOnLoad()
        movies['Search'].forEach(function(movie){
         output += `
         <br>
-        <div class="col-md-4">
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top zoom" src="${movie.Poster}" alt="Poster" style="width:=350px;padding-left:=20px;height:=300px;padding-top:=20px;">
-          <div class="card-body " style="height:=300px">
-          <h5 class="card-title">${movie.Title}</h5>
-          </div>
-          <div class="card-footer">
-          <small class="text-muted ">Released Year :${movie.Year}</small>
-          </div>
-          </div>
+        <div class="flip-card" style="margin-bottom: 300px;margin-left: 30px;">
+          <div class="flip-card-inner">
+              <div class="flip-card-front">
+                <img src="${movie.Poster}" alt="Avatar" style="width:300px;height:450px;">
+              </div>
+            <div class="flip-card-back"  style="width:300px;height:450px;">
+              <h1>${movie.Title}</h1>
+              <p>${movie.Year}</p>
+            </div>
+        </div>
       </div>
         `
       })
 
       document.getElementById('movies').innerHTML = output;
     }
+   
   }
 
   xhr.send();
 
 }
-
-
-
